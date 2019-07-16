bda227ae9b77c5c9dc2122a1f5054f161e021943
ImsirovicAjdin <ajdika@live.com>
2019-07-16 13:57:02 +0200

    remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,3 @@
-// Render an Observable Date with the Async and Date pipes
-
-// To turn this clock into a working clock instead of just pumping out 0 to infinity
-// we need to push out a date (2)
-
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
@@ -10,14 +5,12 @@ import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
-  //templateUrl: './app.component.html',
-  //styleUrls: [ './app.component.css' ]
   template: `
     <h1>{{clock | async | date: 'MMM d, y, h:mm:ss a'}}</h1>
-  ` // the date pipe takes a format string https://angular.io/api/common/DatePipe
+  `
 })
 export class AppComponent  {
   clock = Observable
       .interval(1000)
-      .map(()=> new Date()); // (2) pushing out a date
+      .map(()=> new Date());
 }
